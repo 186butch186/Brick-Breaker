2df4f42adfef28612f6a33b199b70fe424a6e746
Michael Butchko <186butch186@gmail.com>
2017-11-04 13:39:59 -0400

    Updates

--- brickBracker/src/brickBracker/Gameplay.java
+++ brickBracker/src/brickBracker/Gameplay.java
@@ -1,5 +1,14 @@
-package brickBracker;
+/*
+ * Brick Breaker
+ * @Michael Butchko
+ * @version 1.0
+ */
 
+package brickBreaker;
+
+
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -17,7 +26,7 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	//Total bricks left in the game
 	private int totalBricks = 0; 
 	//Timer
-	private Timer time;
+	private Timer timer;
 	//Delay for player to react
 	private int delay = 8;
 	//Players coordinate 
@@ -28,7 +37,33 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	private int ballXDir = -1;
 	private int ballYDir = -2;
 	
+	public Gameplay(){
+		addKeyListener(this);
+		setFocusable(true);
+		setFocusTraversalKeysEnabled(false);
+		//timer = new Timer (delay,this);
+		//timer.start();
+	} 
 	
+	public void paint(Graphics g){
+		//background
+		g.setColor(Color.black);
+		g.fillRect(1,1,692,592);
+		
+		//borders
+		g.setColor(Color.yellow);
+		g.fillRect(0, 0, 3, 592);
+		g.fillRect(0, 0, 692, 3);
+		g.fillRect(691, 0, 3, 592);
+		
+		//paddle
+		g.setColor(Color.green);
+		g.fillRect(playerX, 550, 100, 8);
+		
+		//ball
+		g.setColor(Color.yellow);
+		g.fillRect(playerX, 550, 100, 8);
+	}
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
@@ -37,21 +72,42 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
-		// TODO Auto-generated method stub
+	public void keyPressed(KeyEvent e) {
+		// Right key is pressed
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT ){
+			//Paddle cannot go out of bounds, keep player stationary
+			if(playerX >= 600){
+				playerX = 600;
+			}	else{
+				moveRight();
+				}		
+		}
 		
+		// Left key is pressed
+		if (e.getKeyCode() == KeyEvent.VK_LEFT ){
+			//Paddle cannot go out of bounds, keep player stationary
+			if(playerX < 10){
+				playerX = 10;
+			}	else{
+				moveLeft();
+				}		
+		}
 	}
 
-	@Override
-	public void keyReleased(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	//Method that moves the player right
+	public void moveRight(){
+		play = true;
+		playerX += 20;
 	}
-
-	@Override
-	public void keyTyped(KeyEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	//Method that moves the player left
+	public void moveLeft(){
+		play = true;
+		playerX -= 20;
 	}
+	
+	@Override
+	public void keyReleased(KeyEvent arg0) {}
+	@Override
+	public void keyTyped(KeyEvent arg0) {}
 
 }
