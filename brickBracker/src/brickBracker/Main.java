bea617e90ba6b134d4e25ecbc03f6dca5505ac25
Michael Butchko <186butch186@gmail.com>
2017-11-04 13:40:36 -0400

    Update Main Java

--- brickBracker/src/brickBracker/Main.java
+++ brickBracker/src/brickBracker/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//The new object
 		JFrame obj = new JFrame();
 		Gameplay Gameplay = new Gameplay();
 		obj.setBounds(10, 10, 700, 600);
